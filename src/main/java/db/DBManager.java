b71712e71a0b8530639ea7c0783658ade51a6be7
tushanbarua <tushan@kth.se>
2025-10-04 14:19:57 +0200

    error for sql connection closed after fist try of logging in fixed

--- src/main/java/db/DBManager.java
+++ src/main/java/db/DBManager.java
@@ -6,13 +6,20 @@ import java.sql.SQLException;
 
 public class DBManager {
 
+    static {
+        try {
+            // Ladda MySQL JDBC-drivern (mycket viktigt)
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static final String URL =
             "jdbc:mysql://localhost:3306/webshop?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
     private static final String PASS = "";
 
-    // Inga globala instanser eller Connection här längre!
-    // DriverManager öppnar en ny connection varje gång du anropar den.
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
